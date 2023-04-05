479fb0bc6ac51db5dbcae3c3a44bfcd3f14e249d
MSAnas012 <117428587+MSAnas012@users.noreply.github.com>
2023-04-06 00:03:31 +0530

    Add files via upload

---
+++ Login.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request, session, redirect, url_for
+
+app = Flask(__name__)
+app.secret_key = 'your_secret_key'
+
+@app.route('/')
+def home():
+    if 'username' in session:
+        username = session['username']
+        return render_template('home.html', username=username)
+    else:
+        return redirect(url_for('login'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('home'))
+    else:
+        return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('home'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
